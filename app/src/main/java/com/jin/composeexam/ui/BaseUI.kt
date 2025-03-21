219774b25c4da6564c8d9f846370312cf32ecd39
doanvu20122000 <doanvu20122000@gmail.com>
2025-03-21 18:07:33 +0700

    add base compose: ColumnCenterItem, RowCenterItem

---
+++ app/src/main/java/com/jin/composeexam/ui/BaseUI.kt
@@ -0,0 +1,31 @@
+package com.jin.composeexam.ui
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.ColumnScope
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.RowScope
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+
+@Composable
+fun ColumnCenterItem(modifier: Modifier = Modifier, content: @Composable ColumnScope.() -> Unit) {
+    Column(
+        modifier, horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        content()
+    }
+}
+
+@Composable
+fun RowCenterItem(modifier: Modifier = Modifier, content: @Composable RowScope.() -> Unit) {
+    Row(
+        modifier,
+        horizontalArrangement = Arrangement.Center,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        content()
+    }
+}
